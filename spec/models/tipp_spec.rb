4f7d5dadedd902d12c286de3b348c438d9a3833c
dario-c <dariocavanillas@gmail.com>
2014-08-18 17:45:31 +0200

    refactor and add tests for Tipp

--- spec/models/tipp_spec.rb
+++ spec/models/tipp_spec.rb
@@ -1,48 +1,84 @@
 require 'rails_helper'
-  
+
 RSpec.describe Tipp, :type => :model do
 
-  it "Has a valid factory" do
-    tipp = FactoryGirl.build(:tipp)
+  describe "A valid Tipp..." do
 
-    expect(tipp).to be_valid
+    before(:each) do 
+      @tipp = FactoryGirl.build(:tipp)
+    end
+
+    it "Has a valid factory" do
+      tipp = FactoryGirl.build(:tipp)
+
+      expect(tipp).to be_valid
+    end
+
+    it "Can be created in the Db" do
+      tipp = FactoryGirl.create(:tipp)
+
+      expect(tipp.id).to be_truthy
+    end
+
+    it "has a name" do 
+      expect(@tipp.name).to be_truthy
+    end    
+
+    it "has a streetname" do 
+      expect(@tipp.streetname).to be_truthy
+    end
+
+    it "belongs to a City" do 
+      expect(@tipp.city_id).to eq(1)
+    end
+
+    it "belongs to a User" do 
+      expect(@tipp.user_id).to eq(1)
+    end
+
   end
 
-  it "Can be created in the Db" do
-    tipp = FactoryGirl.create(:tipp)
+  describe "An invalid Tipp..." do
+    it "has no name" do 
+      tipp = FactoryGirl.build(:tipp, name: nil)
+      expect(tipp).to be_invalid
+    end
 
-    expect(tipp.id).to be_truthy
+    it "has no streetname" do
+      tipp = FactoryGirl.build(:tipp, streetname:nil)
+
+      expect(tipp).to be_invalid
+    end    
+
+    it "does not belong to any city" do
+      tipp = FactoryGirl.build(:tipp, city_id:nil)
+
+      expect(tipp).to be_invalid
+    end
+
+    it "does not belong to any user" do
+      tipp = FactoryGirl.build(:tipp, user_id:nil)
+
+      expect(tipp).to be_invalid
+    end
   end
 
-  it "Requires a name" do 
-    tipp = FactoryGirl.build(:tipp, name: nil)
-    expect(tipp).to be_invalid
+  describe "Random Method" do
+
+    before(:each) do 
+      10.times {FactoryGirl.create(:tipp)}
+    end
+
+    it "Gives 10 Tipps" do
+      expect(Tipp.all.count).to be(10)
+    end  
+
+    it "Tipps are Random" do
+      one = Tipp.random_ten
+      two = Tipp.random_ten
+      
+      expect(one==two).to be(false)
+    end
+
   end
-
-  it "Can have a Streetname" do
-    tipp = FactoryGirl.build(:tipp, streetname:"Elm Street")
-
-    expect(tipp.streetname).to eq("Elm Street")
-  end
-
 end
-
-describe "Random Method" do
-
-  before(:each) do 
-    10.times {FactoryGirl.create(:tipp)}
-  end
-
-  it "Gives 10 Tipps" do
-    expect(Tipp.all.count).to be(10)
-  end  
-
-  it "Tipps are Random" do
-    one = Tipp.random_ten
-    two = Tipp.random_ten
-    
-    expect(one==two).to be(false)
-  end
-
-end
-
