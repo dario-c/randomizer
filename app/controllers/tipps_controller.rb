96b53afb16381ce9566f01cd10516aeedcbd8907
dario-c <dariocavanillas@gmail.com>
2014-08-19 11:59:11 +0200

    Flash Notices added + Form Tests

--- app/controllers/tipps_controller.rb
+++ app/controllers/tipps_controller.rb
@@ -13,12 +13,12 @@ skip_before_filter :authenticate_user!, :only => [:index]
     @tipp = Tipp.new(tipp_params)
 
     if @tipp.save
-      redirect_to tipps_path
+      redirect_to tipps_path, :flash => { :notice => "Thanks for your Tipp!"}
 
     else 
       @error = @tipp.errors.full_messages
 
-      redirect_to new_tipp_path, :flash => { :error => "Sorry, your data doesnt seem to be correct.Please check again that you added all the required fields"}
+      redirect_to new_tipp_path, :flash => { :error => "Sorry, your data doesnt seem to be correct. #{@error}"}
     end
 
 
--- spec/controllers/tipps_controller_spec.rb
+++ spec/controllers/tipps_controller_spec.rb
@@ -1,22 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe TippsController, :type => :controller do
-  
-  describe "User Logged in" do   
-    it "renders the index template" do
+   
+  describe "If User is logged in..." do 
+    before(:each) do  
       user = FactoryGirl.create(:user)
       sign_in user   
+    end
+
+    it "Is directed to the Index" do
       get :index
       expect(response).to render_template("index")
     end
-  end
-
-  describe "User Not Logged in" do   
-    it "Redirects to log in page" do
-      get :new
-      expect(response).to redirect_to(new_user_session_path)
+    
+    it "is showed the New form" do
+      get :new 
+      expect(response).to render_template("new")   
     end
+
+    context"when creating a new Tipp..."do
+
+      it "can create a correct Tipp" do
+        post :create, tipp: {name:"A Place", streetname:"A Street", user_id: 1, city_id: 1}
+        expect(flash[:notice]).to match("Thanks for your Tipp!") 
+        expect(response).to redirect_to(tipps_path)
+      end
+
+     it "is noticed when form is incorrect" do 
+      post :create, tipp: {name:"A Place", streetname:"A Street", user_id: nil, city_id: 1}
+      expect(flash[:error]).to be_truthy
+     end
+
+     it "redirected back when form is incorrect" do 
+      post :create, tipp: {name:nil, streetname:"A Street", user_id: 1, city_id: 1}
+      expect(response).to redirect_to(new_tipp_path)
+     end
+
+   end
+
+    end 
+
+  describe "If user is not logged in..." do
+    it "is directed to the Index" do
+    get :index
+    expect(response).to render_template("index")
+    end
+
+    context "it is redirected to new_user_path when..." do
+      it "tries to create a new Tipp" do
+        get :new 
+        expect(response).to redirect_to(new_user_session_path)
+      end   
+    end    
   end
 
-
+#expect(response).to redirect_to(new_user_session_path)
+    
 end  
