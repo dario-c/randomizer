cb45967c024134a38210a27b63ab266ec439c135
dario-c <dariocavanillas@gmail.com>
2014-08-20 09:27:50 +0200

    Changed Delete empty comments methods

--- app/controllers/tipps_controller.rb
+++ app/controllers/tipps_controller.rb
@@ -14,7 +14,8 @@ skip_before_filter :authenticate_user!, :only => [:index]
     @tipp = Tipp.new(tipp_params)
 
     if @tipp.save
-      redirect_to tipps_path, :flash => { :notice => "Thanks for your Tipp!"}
+     
+    redirect_to tipps_path, :flash => { :notice => "Thanks for your Tipp!"}
     else 
       @error = @tipp.errors.full_messages
       redirect_to new_tipp_path, :flash => { :error => "Sorry, your data doesnt seem to be correct. #{@error}"}
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,22 +8,25 @@ class Comment < ActiveRecord::Base
 
   private
 
-  def delete_empty_comments
-    
-    comment = Comment.last
-
-    if comment.content && comment.content.empty?
-      comment.destroy!.save
+  def delete_empty_comments    
+    unless self.content
+      self.destroy
     end
   end
 
   def delete_not_related_comments
 
-    comment = Comment.last
-    if comment.user_id && comment.tipp_id
-      if comment.user_id.empty? || comment.tipp_id.empty?
-        comment.destroy!.save
-      end
-    end
+    # comment = Comment.last
+    # if comment.user_id && comment.tipp_id
+    #   if comment.user_id.empty? || comment.tipp_id.empty?
+    #     comment.destroy
+    #   end
+    # end
   end
 end
+
+  
+ # comment = Comment.where(tipp_id:@tipp.id)[0]
+ #     if comment.content.empty?
+ #      comment.destroy
+ #     end
