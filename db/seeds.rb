c64ed2548e906c72ab0d705d95092eda3749473c
dario-c <dariocavanillas@gmail.com>
2014-09-16 12:20:14 +0200

    added new seeds and deleted extra images

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+barcelona = City.create!(name:'barcelona')
+
+randomizer = User.create!(name: "randomizer", role: "ambassador", karma: 100, email: "abc3@abc.com", password: "qwerty123" )
+
+peter = User.create!(name: "peter", role: "ambassador", karma: 106, email: "abc@abc.com", password: "qwerty123")
+
+jane = User.create!(name: "jane", role: "regular", karma: 10, email: "abc2@abc.com", password: "qwerty123")
+
+
+macba = Tipp.create!(city: barcelona, user: peter, name: "MACBA",streetname: "Plaza dels Angels 1", website: "http://www.macba.cat/en/", twitter: "MACBA_Barcelona", points: 100, offer: false, image: File.open(File.join(Rails.root, '/app/assets/images/macba.jpg')))
+
+soulkitchen = Tipp.create!(city: barcelona, user: peter, name: "soulkitchen",streetname: "Plaza de Sant Pere 10", website: "https://www.facebook.com/soulkitchen.santpere/timeline", twitter: "kreuzberg75", points: 100, offer: false, image: File.open(File.join(Rails.root, '/app/assets/images/soulkitchen.jpg')))
+
+oval = Tipp.create!(city: barcelona, user: peter, name: "oval",streetname: "Carrer de Valencia 199", website: "http://www.ovalbcn.com/", twitter: "Oval_Bcn", points: 100, offer: false, image: File.open(File.join(Rails.root, '/app/assets/images/oval.jpg')))
+
+sidecar = Tipp.create!(city: barcelona, user: jane, name: "sidecar",streetname: "Plaza Reial 7", website: "http://www.sidecarfactoryclub.com/", twitter: "SIDECARbcn", points: 10, offer: false, image: File.open(File.join(Rails.root, '/app/assets/images/sidecar.jpg')))
+
+heliogabal = Tipp.create!(city: barcelona, user: jane, name: "heliogabal",streetname: "Carrer Ramon y Cajal 80", website: "http://www.heliogabal.com/", twitter: "HeliogabalBcn", points: 10, offer: false, image: File.open(File.join(Rails.root, '/app/assets/images/heliogabal.jpg')))
+
+mosquito = Tipp.create!(city: barcelona, user: jane, name: "mosquito",streetname: "Carrer Carders 46", website: "http://www.mosquitotapas.com/", points: 10, offer: false, image: File.open(File.join(Rails.root, '/app/assets/images/mosquito.jpg')))
+
+nomad = Tipp.create!(city: barcelona, user: randomizer, name: "nomad coffee",streetname: "Pasaje de Sert", website: "https://www.facebook.com/nomadcoffeeproductions", points: 100, offer: true, image: File.open(File.join(Rails.root, '/app/assets/images/nomad.jpg')))
+
+
+
 # city = City.create(name:"barcelona")
 
 
