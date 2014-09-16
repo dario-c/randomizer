d7a724b049706ea9dffd11294197106081100eff
dario-c <dariocavanillas@gmail.com>
2014-09-16 13:08:19 +0200

    Tipps are shuffled and minor refactor

--- app/controllers/tipps_controller.rb
+++ app/controllers/tipps_controller.rb
@@ -3,12 +3,14 @@ skip_before_filter :authenticate_user!, :only => [:index]
 
   def index    
     cookies.delete :choices
+    
     unless cookies[:choices]
       todays_choices =  Tipp.random_five(user_signed_in?)
       cookies[:choices] = { value: JSON.generate(todays_choices), expires: 24.hours.from_now }
     end
+    
     cookie = JSON.parse(cookies[:choices])  
-    @random_choices = Tipp.find_these(cookie) 
+    @random_choices = Tipp.find_these(cookie).shuffle 
   end
 
   def new
@@ -20,13 +22,11 @@ skip_before_filter :authenticate_user!, :only => [:index]
     @tipp = Tipp.new(tipp_params)
             
     if @tipp.save
-
       @user = User.find(@tipp.user_id)
       @user.update_karma("created")
       @tipp.update_points("created", @user.role)
       
       redirect_to new_tipp_path, :flash => {:addcomment => "", :tipp => @tipp.name} 
-    
     else 
       @errors = @tipp.errors.full_messages
       @error = @tipp.errors[:name]
--- app/models/tipp.rb
+++ app/models/tipp.rb
@@ -24,18 +24,17 @@ class Tipp < ActiveRecord::Base
   end
 
   def self.find_these(array)
-    Tipp.where({id: array})
+    Tipp.where({ id: array })
   end
 
 
   def update_points(action, role)
     new_ammount = 0
 
-
-    points = { created:  { badkarma: 0, regular: 10, ambassador: 100 },
-              commented: { badkarma: 0, regular: 5, ambassador: 10 },
-              was_upvoted:  { badkarma: 0, regular: 1, ambassador: 1 },
-              was_downvoted: { badkarma: 0, regular: -1, ambassador: -1 }}
+    points = { created:       { badkarma: 0, regular: 10, ambassador: 100 },
+              commented:      { badkarma: 0, regular: 5,  ambassador: 10 },
+              was_upvoted:    { badkarma: 0, regular: 1,  ambassador: 1 },
+              was_downvoted:  { badkarma: 0, regular: -1, ambassador: -1 }}
 
     new_ammount = points[action.to_sym][role.to_sym]
     self.points += new_ammount
@@ -43,14 +42,8 @@ class Tipp < ActiveRecord::Base
   end
 
   def random_comment
-
     Comment.random_comment(self.id)
-
   end
-
-
-
-
 
 end
 
